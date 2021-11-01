18523ecaf6f3565731ec34b5beed09b947e4625a
Nara Roth <nara.achanzar@gmail.com>
2021-11-01 15:43:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ for(let i=0;i <weeklyExpenseQuestions.length;i++){
 let answer = parseFloat (window.prompt(weeklyExpenseQuestions[i]))
 weeklyExpenses = weeklyExpenses + answer
 }
+for(let i=0;i <monthlyExpenseQuestions.length;i++){ 
+    let answer = parseFloat (window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = weeklyExpenses + answer
+}
+for(let i=0;i <weeklyExpenseQuestions.length;i++){ 
+        let answer = parseFloat (window.prompt(weeklyExpenseQuestions[i]))
+        weeklyExpenses = weeklyExpenses + answer
+}
